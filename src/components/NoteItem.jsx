d834f339be36d6b2aa080066c51e5c568f3b657d
Kiran Khayamali <kirankhayamali@gmail.com>
2025-11-13 20:38:27 +0545

    Making note item for easier deletion

---
+++ src/components/NoteItem.jsx
@@ -0,0 +1,13 @@
+function NoteItem({ id, text, date, handleDeleteNote }) {
+    return (
+        <div className="note-item">
+            <p className="note-text">{text}</p>
+            <div className="note-item-footer">
+                <small>{date}</small>
+                <button className="delete-button" onClick={() => handleDeleteNote(id)}>Delete</button>
+            </div>
+        </div>
+    )
+};
+
+export default NoteItem;
