39fe51ea35c06bd31caf38b9189eb4a88ea1ea88
Kiran Khayamali <kirankhayamali@gmail.com>
2025-11-16 17:38:36 +0545

    Adding pin notes feature

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,7 +1,7 @@
 import NoteItem from "./NoteItem";
 import '../styles/NoteList.css';
 
-export default function NoteList({notes, handleDeleteNote, handleEditNote}) {
+export default function NoteList({notes, handleDeleteNote, handleEditNote, handlePinNote}) {
     return (
         <div className="note-list">
             {notes.map((note) => (
@@ -11,8 +11,10 @@ export default function NoteList({notes, handleDeleteNote, handleEditNote}) {
                     text={note.text}
                     date={note.date}
                     color={note.color}
+                    isPinned={note.isPinned}
                     handleDeleteNote={handleDeleteNote}
                     handleEditNote={handleEditNote}
+                    handlePinNote={handlePinNote}
                 />
             ))}
         </div>   
