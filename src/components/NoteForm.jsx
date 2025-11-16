7044f11a2edceb6acdb3d5d0b81f6ac9b55a1427
Kiran Khayamali <kirankhayamali@gmail.com>
2025-11-16 17:24:38 +0545

    Adding color circle for color options

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -3,6 +3,7 @@ import '../styles/NoteForm.css';
 
 function NoteForm({ onAddNote }) {
     const [noteText, setNoteText] = useState("");
+    const [color, setColor] = useState("#fff8b5");
     const characterLimit = 200;
 
     const handleChange = (event) => {
@@ -19,13 +20,25 @@ function NoteForm({ onAddNote }) {
     };
 
     return (
-        <div className="note-form">
+        <div className="note-form" style={ {backgroundColor: color}}>
             <textarea
                 rows="4"
                 placeholder="Write your note here...."
                 value={noteText}
                 onChange={handleChange}
             ></textarea>
+
+            <div className="color-picker">
+                {["#fff8b5", "#ffadad", "#ffd6a5", "#caffbf", "#9bf6ff", "#a0c4ff", "#bdb2ff"].map((c) => (
+                    <span
+                        key={c}
+                        className={`color-circle ${color === c ? "selected" : ""}`}
+                        style={{ backgroundColor: c }}
+                        onClick={() => setColor(c)}
+                    ></span>
+                ))}
+            </div>
+
             <div className="note-footer">
                 <small>{characterLimit - noteText.length} Remaining</small>
                 <button className="save-button" onClick={handleSubmit}>Save</button>
